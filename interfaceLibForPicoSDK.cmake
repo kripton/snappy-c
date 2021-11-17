e76ca8448ae53760659f956ba15695e450820e23
Jannis Achstetter <kripton@kripserver.net>
2021-11-17 22:16:37 +0100

    Add cmake-file for pico-sdk based projects

---
+++ interfaceLibForPicoSDK.cmake
@@ -0,0 +1,19 @@
+## Include this file if you want to use the snappy-c library
+## in YOUR (Pico) project.
+
+cmake_minimum_required(VERSION 3.12)
+
+# Define the snappyc library
+add_library(snappyc INTERFACE)
+
+target_sources(snappyc INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/fuzzer.c
+    ${CMAKE_CURRENT_LIST_DIR}/glue.c
+    ${CMAKE_CURRENT_LIST_DIR}/map.c
+    ${CMAKE_CURRENT_LIST_DIR}/snappy.c
+    ${CMAKE_CURRENT_LIST_DIR}/util.c
+)
+
+target_include_directories(snappyc INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/
+)
